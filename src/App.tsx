76da55ef2f7236ea02f80a952830c624e7b45988
Shiv Yadav <shivyadav0014@gmail.com>
2026-02-09 23:30:44 +0545

    app component

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import {ShiftingDropDown} from "./components/dropdown/ShiftingDropDown";
+const App = () => {
+  return (
+    <div className='App'>
+      <ShiftingDropDown />
+    </div>
+  );
+};
+
+export default App;
